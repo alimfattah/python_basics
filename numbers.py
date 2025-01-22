b8a8afd93de434407f585c91320d3863796aae39
Fattahul Alim Muhamad Abdi <140358363+alimfattah@users.noreply.github.com>
2025-01-22 21:21:37 +0700

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,16 @@
+print("Integers")
+#Whole numbers only
+#Positive Or Negative
+#No Decimal Points
+print("9, 378, -21\nFloats")
+#Written with a Decimal Point
+#Positive Or Negative
+print("1.5,9.99,-2.0")
+
+
+
+#Check type of number(type(number))
+print(type(6))
+
+#Separate in large number notation use _
+print(31_000_000_000)
