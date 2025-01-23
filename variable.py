473313fd04b08af26e4ce9d6eb7ebcded869b975
Fattahul Alim Muhamad Abdi <140358363+alimfattah@users.noreply.github.com>
2025-01-23 21:17:09 +0700

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,9 @@
+#Variable are like labels for values
+age=47
+#variable, Assignment operator, Value
+print(age)
+age = age+ 1
+print(age)
+help("keywords")
+#assignment operator
+#+=,-=, *=, /=, //=, **=, %=
