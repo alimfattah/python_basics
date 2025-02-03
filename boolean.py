81dc0d65a213d3dc831de20c3f6fd75c07afaad7
Fattahul Alim Muhamad Abdi <140358363+alimfattah@users.noreply.github.com>
2025-02-03 17:26:16 +0700

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,40 @@
+#bool False- Y
+#False, 0.0, 0, None, range(0), Empty strings (''), empty Daya Struvtures (), {}, set(),[]
+#Truthy
+#Everything else
+print(bool(0))
+print(bool(43324))
+print(bool(-1))
+print(bool(0.0))
+print(bool('asd'))
+print(bool('False'))
+print(bool(""""""))
+print(bool("""jonds"""))
+print(bool([]))
+print(bool([1]))
+#boolean with operator
+print("A" in "abba")
+print("A" in "abbA")
+print("bb" in "abbA")
+#not iterable in int
+#print(3 in 312)
+#list
+print(4 in [4,5,99])
+#less than operator <
+print('a' < 'w')
+print('a' < '$')
+print('aaa' < 'AAA')
+print('A' < 'a')
+#ORD() Phyton uses unicode for encoding characters used in string data type
+# The function ord() will return the number for any characters
+print(ord('a'))
+print(ord('A'))
+print(ord('B'))
+print(ord('C'))
+print(ord('1'))
+print(ord(' '))
+print(ord('.'))
+#string
+print('100' < '9')
+print(ord('1'))
+print(ord('9'))
