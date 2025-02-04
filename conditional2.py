c262b9ed6ae06f261698e2e24388208b6c39075f
Fattahul Alim Muhamad Abdi <140358363+alimfattah@users.noreply.github.com>
2025-02-04 20:41:10 +0700

    Update conditional2.py

--- conditional2.py
+++ conditional2.py
@@ -32,14 +32,3 @@ if num<10:
     print("form the if 3")
 
 
-first_name = input ("Enter your first name : ")
-last_name = input ("enter your last name:")
-name_length = len(first_name) +len(last_name)
-
-if name_length ==12:
-    print(f"{first_name} {last_name} is exactly the average of the american names") 
-
-elif name_length < 12:
-    print(f"{first_name} {last_name} is shorter than the average of the american names") 
-else:
-    print(f"{first_name} {last_name} is longer than the average of the american names") 
